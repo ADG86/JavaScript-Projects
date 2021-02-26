1485e1514d5558a256c6ffa0e5248104401f1464
ADG86 <duncangeisler86@gmail.com>
2021-02-26 01:21:13 -0800

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,14 @@
+function myFunction(p1, p2) {
+   return p1 * p2;
+}
+  document.getElementById("demo").innerHTML = myFunction(4, 6);
+
+
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "Hello World";
+}
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
