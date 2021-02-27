e96c1240d8b0669bb9c0e8c68551ffdf99e0923b
ADG86 <duncangeisler86@gmail.com>
2021-02-27 13:19:45 -0800

    Add files via upload

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -4,7 +4,7 @@ document.write("I am writing something."); //Basic way to write in JavaScript
 
 document.write("<br>"); //How to page break in JavaScript
 
-document.write("\"I am writing a quote "
+document.write("\"I am writing a quote " //Notice the "\" to make sure there is no confusion for quote marks
 + "that is to be concatenating," 
 + " because I didn\'t know how to before.\"") //Concatenating
 
@@ -22,18 +22,32 @@ var Son = Son.fontcolor("orange");
 document.write(Family //Also a little concatenating
 + Son);
 
+document.write("<br>");
+
 document.write(3 / (3 + 3)); //An expression with more complex math
 
 document.write("<br>");
 
-function myFunction() { //Similiar funcations need to point to a specific id or they will override them
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function date() { 
+    document.getElementById('demo').innerHTML = Date();
 }
 
-function myFunction1() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate1").innerHTML = sentence;
+function myFunction() { //Similiar functions need to point to a specific id or they will override them
+    var str = "I am learning";
+    str += " a lot from this book!";
+
+    var result = str.fontcolor("blue");
+
+    document.getElementById("Concatenate").innerHTML = result;
 }
+
+function myFunction1() { //the function name calls the executable function
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("Concatenate1").innerHTML = sentence; //the id name puts the function where we want it
+}
+
+document.write("<br>")
+
+document.write("hello world");
+
